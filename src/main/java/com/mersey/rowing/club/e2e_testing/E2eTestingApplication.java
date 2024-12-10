8706e2643ecd65e29b5e080e1b92215d3f61cf9e
samuellsrobertson <samlsrobertson@gmail.com>
2024-12-10 15:20:12 +0000

    - initial commit

---
+++ src/main/java/com/mersey/rowing/club/e2e_testing/E2eTestingApplication.java
@@ -0,0 +1,13 @@
+package com.mersey.rowing.club.e2e_testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E2eTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(E2eTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mersey/rowing/club/e2e_testing/E2eTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mersey.rowing.club.e2e_testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class E2eTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
